47bff14c56d575581a45d9db150fc2c7d452a5dc
gilvan <g.r.almeida@live.com>
2020-02-06 22:46:28 -0300

    Action results and response formatters

--- MyAPICore/MyAPICore/Controllers/ValuesController.cs
+++ MyAPICore/MyAPICore/Controllers/ValuesController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyAPICore.Controllers
@@ -14,7 +16,36 @@ namespace MyAPICore.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var values = new string[] { "value1", "value2" };
+
+            if (values.Length < 5000)
+                return BadRequest();
+
+            return Ok(values);
+        }
+
+        // GET api/values/get-result
+        [HttpGet("get-result")]
+        public ActionResult GetResult()
+        {
+            var values = new string[] { "value1", "value2" };
+
+            if (values.Length < 5000)
+                return BadRequest();
+
+            return Ok(values);
+        }
+
+        // GET api/values/get-values
+        [HttpGet("get-values")]
+        public IEnumerable<string> GetValues()
+        {
+            var values = new string[] { "value1", "value2" };
+
+            if (values.Length < 5000)
+                return null;
+
+            return values;
         }
 
         // GET api/values/5
@@ -30,14 +61,46 @@ namespace MyAPICore.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        [ProducesResponseType(typeof(Product), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult Post(Product product)
         {
+            if (product.Id == 0) return BadRequest();
+
+            // add database
+
+            return CreatedAtAction("Post", product);
+        }
+
+        // POST api/values/product/from-query?id=10&name=Name&description=Description
+        [HttpPost("product/from-query")]
+        public ActionResult<Product> Post([FromQuery] int id, [FromQuery] string name, [FromQuery] string description)
+        {
+            if (id == 0) return BadRequest();
+            if (name.Length < 5) return BadRequest();
+            if (description.Length < 10) return BadRequest();
+
+            var product = new Product
+            {
+                Id = id,
+                Name = name,
+                Description = description
+            };
+
+            return Ok(product);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put([FromRoute] int id, [FromBody] string value)
         {
+        }
+
+        // PUT api/values/product/5
+        [HttpPut("product/{id:int}")]
+        public void Put([FromRoute] int id, [FromForm] Product product)
+        {
+
         }
 
         // DELETE api/values/5
@@ -45,5 +108,16 @@ namespace MyAPICore.Controllers
         public void Delete(int id)
         {
         }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
     }
 }
