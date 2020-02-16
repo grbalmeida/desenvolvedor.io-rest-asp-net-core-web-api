d7077dcf5a1eb1bf76af6a50cb918d75ef761cf8
gilvan <g.r.almeida@live.com>
2020-02-16 11:24:56 -0300

    Product registration and image upload

---
+++ MyFullAPI/src/DevIO.Api/Controllers/ProductsController.cs
@@ -0,0 +1,105 @@
+﻿using AutoMapper;
+using DevIO.App.ViewModels;
+using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/products")]
+    public class ProductsController : MainController
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IProductRepository productRepository,
+                                  IProductService productService,
+                                  IMapper mapper,
+                                  INotifier notifier) : base(notifier)
+        {
+            _productRepository = productRepository;
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProductViewModel>> GetAll()
+        {
+            return _mapper.Map<IEnumerable<ProductViewModel>>(await _productRepository.GetProductsAndSuppliers());
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ProductViewModel>> GetById(Guid id)
+        {
+            var productViewModel = await GetProduct(id);
+
+            if (productViewModel == null) return NotFound();
+
+            return productViewModel;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductViewModel>> Add(ProductViewModel productViewModel)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var imageName = Guid.NewGuid() + "_" + productViewModel.Image;
+
+            if (!UploadFile(productViewModel.UploadImage, imageName))
+            {
+                return CustomResponse(productViewModel);
+            }
+
+            productViewModel.Image = imageName;
+
+            await _productService.Add(_mapper.Map<Product>(productViewModel));
+
+            return CustomResponse(productViewModel);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<ProductViewModel>> Delete(Guid id)
+        {
+            var product = await GetProduct(id);
+
+            if (product == null) return NotFound();
+
+            await _productService.Remove(id);
+
+            return CustomResponse(product);
+        }
+
+        private bool UploadFile(string file, string imageName)
+        {
+            var imageDataByteArray = Convert.FromBase64String(file);
+
+            if (string.IsNullOrEmpty(file))
+            {
+                NotifyError("Please provide an image for this product!");
+                return false;
+            }
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", imageName);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                NotifyError("There is already a file with that name!");
+                return false;
+            }
+
+            System.IO.File.WriteAllBytes(filePath, imageDataByteArray);
+
+            return true;
+        }
+
+        private async Task<ProductViewModel> GetProduct(Guid id)
+        {
+            return _mapper.Map<ProductViewModel>(await _productRepository.GetProductAndSupplier(id));
+        }
+    }
+}
