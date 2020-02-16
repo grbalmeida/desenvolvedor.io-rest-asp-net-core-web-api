af91c958af799acc84036fc49b8b10ad7492ee2b
gilvan <g.r.almeida@live.com>
2020-02-16 10:19:03 -0300

    Standardizing validation and business errors

--- MyFullAPI/src/DevIO.Api/Controllers/MainController.cs
+++ MyFullAPI/src/DevIO.Api/Controllers/MainController.cs
@@ -1,10 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Notifications;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Linq;
 
 namespace DevIO.Api.Controllers
 {
     [ApiController]
     public abstract class MainController : ControllerBase
     {
-        
+        private readonly INotifier _notifier;
+
+        public MainController(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
+
+        protected bool ValidOperation()
+        {
+            return !_notifier.HasNotification();
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (ValidOperation())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notifier.GetNotifications().Select(n => n.Message)
+            });
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid) NotifyInvalidModelError(modelState);
+
+            return CustomResponse();
+        }
+
+        protected void NotifyInvalidModelError(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+
+            foreach (var error in errors)
+            {
+                var errorMessage = error.Exception == null ? error.ErrorMessage : error.Exception.Message;
+
+                NotifyError(errorMessage);
+            }
+        }
+
+        protected void NotifyError(string errorMessage)
+        {
+            _notifier.Handle(new Notification(errorMessage));
+        }
     }
 }
--- MyFullAPI/src/DevIO.Api/Controllers/SuppliersController.cs
+++ MyFullAPI/src/DevIO.Api/Controllers/SuppliersController.cs
@@ -14,14 +14,18 @@ namespace DevIO.Api.Controllers
     {
         private readonly ISupplierRepository _supplierRepository;
         private readonly ISupplierService _supplierService;
+        private readonly IAddressRepository _addressRepository;
         private readonly IMapper _mapper;
 
         public SuppliersController(ISupplierRepository supplierRepository,
                                    ISupplierService supplierService,
-                                   IMapper mapper)
+                                   IAddressRepository addressRepository,
+                                   IMapper mapper,
+                                   INotifier notifier) : base(notifier)
         {
             _supplierRepository = supplierRepository;
             _supplierService = supplierService;
+            _addressRepository = addressRepository;
             _mapper = mapper;
         }
 
@@ -44,44 +48,57 @@ namespace DevIO.Api.Controllers
         [HttpPost]
         public async Task<ActionResult<SupplierViewModel>> Add(SupplierViewModel supplierViewModel)
         {
-            if (!ModelState.IsValid) return BadRequest();
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
 
-            var supplier = _mapper.Map<Supplier>(supplierViewModel);
+            await _supplierService.Add(_mapper.Map<Supplier>(supplierViewModel));
 
-            var result = await _supplierService.Add(supplier);
-
-            if (!result) return BadRequest();
-
-            return Ok();
+            return CustomResponse(supplierViewModel);
         }
 
         [HttpPut("{id:guid}")]
         public async Task<ActionResult<SupplierViewModel>> Update(Guid id, SupplierViewModel supplierViewModel)
         {
-            if (id != supplierViewModel.Id) return BadRequest();
+            if (id != supplierViewModel.Id)
+            {
+                NotifyError("The id given is not the same as the one entered in the query");
+                return CustomResponse(supplierViewModel);
+            }
 
-            if (!ModelState.IsValid) return BadRequest();
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
 
-            var supplier = _mapper.Map<Supplier>(supplierViewModel);
-            var result = await _supplierService.Update(supplier);
+            await _supplierService.Update(_mapper.Map<Supplier>(supplierViewModel));
 
-            if (!result) return BadRequest();
-
-            return Ok(supplier);
+            return CustomResponse(supplierViewModel);
         }
 
         [HttpDelete("{id:guid}")]
         public async Task<ActionResult<SupplierViewModel>> Delete(Guid id)
         {
-            var supplier = await GetSupplierAddress(id);
+            var supplierViewModel = await GetSupplierAddress(id);
 
-            if (supplier == null) return NotFound();
+            if (supplierViewModel == null) return NotFound();
 
-            var result = await _supplierService.Remove(id);
+            await _supplierService.Remove(id);
 
-            if (!result) return BadRequest();
+            return CustomResponse(supplierViewModel);
+        }
 
-            return Ok(supplier);
+        [HttpGet("get-address/{id:guid}")]
+        public async Task<AddressViewModel> GetAddressById(Guid id)
+        {
+            return _mapper.Map<AddressViewModel>(await _addressRepository.GetById(id));
+        }
+
+        [HttpPut("update-address/{id:guid}")]
+        public async Task<IActionResult> UpdateAddress(Guid id, AddressViewModel addressViewModel)
+        {
+            if (id != addressViewModel.Id) return BadRequest();
+
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            await _supplierService.UpdateAddress(_mapper.Map<Address>(addressViewModel));
+
+            return CustomResponse(addressViewModel);
         }
 
         public async Task<SupplierViewModel> GetAddressAndProductsFromSupplier(Guid id)
