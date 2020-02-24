3ae5d58fd6e7d53fd82c78af2b77590af64b77d5
gilvan <g.r.almeida@live.com>
2020-02-24 20:05:44 -0300

    JWT authorization in Swagger

--- MyFullAPI/src/DevIO.Api/Configuration/SwaggerConfig.cs
+++ MyFullAPI/src/DevIO.Api/Configuration/SwaggerConfig.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DevIO.Api.Configuration
@@ -15,6 +16,21 @@ namespace DevIO.Api.Configuration
             services.AddSwaggerGen(c =>
             {
                 c.OperationFilter<SwaggerDefaultValues>();
+
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    { "Bearer", new string[] { } }
+                };
+
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "Insert the JWT token like this: Bearer {your token}",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+
+                c.AddSecurityRequirement(security);
             });
 
             return services;
